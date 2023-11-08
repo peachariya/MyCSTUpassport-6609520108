531dd5cfa92e3c5bd8f0840c47bebaeed0a84522
peachariya <ariya.tang@dome.tu.ac.th>
2023-11-09 03:26:21 +0700

    htmlandcss commit

---
+++ cs102Hw3-1-6690520108.c
@@ -0,0 +1,15 @@
+cs102Hw3-1-6690520108.c
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    printf("Enter size of your rectangle:");
+    i=x
+    j=o
+    for (i=0; i<j; i++)
+    {
+        for (j=0 i>j; j++)
+            printf("%c",j)
+    }
+    rutun 0;
+}
